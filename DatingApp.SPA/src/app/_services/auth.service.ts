5827cefb3803cfa4fb4ba5e422e042d623d0c637
Saul Rodriguez <saulenriquejr@gmail.com>
2018-04-09 16:34:24 -0500

    added exception handling to angular authorization service

--- DatingApp.SPA/src/app/_services/auth.service.ts
+++ DatingApp.SPA/src/app/_services/auth.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, RequestOptions, Headers, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class AuthService {
@@ -16,15 +19,36 @@ constructor(private http: Http) { }
                 localStorage.setItem('token', user.tokenString);
                 this.userToken = user.tokenString;
             }
-        });
+        }).catch(this.handleError);
     }
 
     register(model: any) {
-        return this.http.post(this.baseUrl + '/register', model, this.getRequestOptions());
+        return this.http.post(this.baseUrl + '/register', model, this.getRequestOptions()).catch(this.handleError);
     }
 
     getRequestOptions() {
         const headers = new Headers({'Content-type': 'application/json'});
         return new RequestOptions({ headers: headers });
+    }
+
+    private handleError(error: any) {
+        const applicationError = error.headers.get('Application-error');
+        if (applicationError) {
+            return Observable.throw(applicationError);
+        }
+
+        const serverError = error.json();
+        let modelStateErrors = '';
+        if (serverError) {
+            for (const key in serverError) {
+                if (serverError[key]) {
+                    modelStateErrors += serverError[key] + '\n';
+                }
+            }
+        }
+
+        return Observable.throw(
+            modelStateErrors || 'Server Error'
+        );
     }
 }
